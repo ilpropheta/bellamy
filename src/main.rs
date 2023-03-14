3d2098b489f2ad4ff4e78a083819e20bc8d22c49
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2023-03-14 21:01:29 +0100

    Cli arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,58 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
-use wry::application::window::Fullscreen;
-
-fn main() -> wry::Result<()> {
-    use wry::{
-      application::{
+use std::error::Error;
+use wry::{
+    webview::WebViewBuilder,
+    application::{
         event::{Event, StartCause, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
-        window::WindowBuilder,
-      },
-      webview::WebViewBuilder,
-    };
-  
+        window::{WindowBuilder, Fullscreen},
+    },
+};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    /// URL to open
+    url: String,
+
+    /// Create borderless fullscreens on current monitor
+    #[arg(long)]
+    fullscreen: bool,
+
+    /// Window title
+    #[arg(long, default_value = "Bellamy")]
+    title: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-      .with_title("Bellamy")
-      .with_fullscreen(Some(Fullscreen::Borderless(None)))
-      .build(&event_loop)?;
+        .with_title(cli.title)
+        .with_fullscreen(match cli.fullscreen {
+            true => Some(Fullscreen::Borderless(None)),
+            false => None
+        })
+        .build(&event_loop)?;
+
     let _webview = WebViewBuilder::new(window)?
-      .with_url("https://en.wikipedia.org/wiki/Samuel_Bellamy")?
-      .build()?;
-  
+        .with_url(&cli.url)?
+        .build()?;
+
     event_loop.run(move |event, _, control_flow| {
-      *control_flow = ControlFlow::Wait;
-  
-      match event {
-        Event::NewEvents(StartCause::Init) => println!("Bellamy has started!"),
-        Event::WindowEvent {
-          event: WindowEvent::CloseRequested,
-          ..
-        } => *control_flow = ControlFlow::Exit,
-        _ => (),
-      }
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::NewEvents(StartCause::Init) => {
+                println!("Bellamy has started!")
+            }
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+                *control_flow = ControlFlow::Exit
+            }
+            _ => (),
+        };
     });
-  }
+}
